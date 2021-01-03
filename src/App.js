9e019169a345a92859b1eec23c7b985393433906
KimChanSik <davidmim99@gmail.com>
2021-01-03 13:28:45 +0900

    My first React Web site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import axios from "axios";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
-class App extends React.Component{
-  state = {
-    isLoading: true,
-    movies: []
-  };
-  getMovies = async () => {
-    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json")
-  }
- componentDidMount(){
-   this.getMovies();
- }
-  render() {
-    const { isLoading } = this.state;
-    return (
-      <div>{isLoading ? "Loading..." : "We are ready"}</div>
-    );
-  }
+function App(){
+  return (
+    <HashRouter>
+      <Navigation/>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
+  );
 }
 
 export default App;
